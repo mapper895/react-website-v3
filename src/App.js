bb3638ede9ccc4eac38f8c38661920c07843a4ed
mapper895 <mapper.895.mp@gmail.com>
2021-03-15 18:31:49 -0600

    Navbar

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import React from "react";
+import GlobalStyle from "./globalStyles";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import { Navbar } from "./components";
 
 function App() {
   return (
-    <div className="App">
-      <h1>App</h1>
-    </div>
+    <Router>
+      <GlobalStyle />
+      <Navbar />
+    </Router>
   );
 }
 
